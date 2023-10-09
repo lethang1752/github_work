3ad3c55e00c2b8bdceacf081525613a73fc04ac7
lethang1752 <79958184+lethang1752@users.noreply.github.com>
2023-10-09 20:37:17 +0700

    a

--- crossplatform_backup_restore/README.txt
+++ crossplatform_backup_restore/README.txt
@@ -5,6 +5,6 @@
 - Run initial_restore.rman with command:
 rman target / @initial_restore.rman > rman_output_restore_full.log
 
-- After restore done, run create_foreign_table.sh
+- After restore done, run target_foreign_datafile.sh
 
 -
--- crossplatform_backup_restore/target_foreign_datafile.sh
+++ crossplatform_backup_restore/target_foreign_datafile.sh
@@ -12,7 +12,12 @@ fi
 sh migrate.properties
 
 sqlplus -S "/ as sysdba" << !
-create table foreign_datafile(file_id int, name varchar(200)) tablespace system;
+create table foreign_datafile
+(
+	file_id int not null, 
+	name varchar(200),
+	constraint file_id_pk primary key (file_id)
+) tablespace system;
 !
 
 cat rman_output_restore_full.log | $grep 'restoring foreign file' | $grep '+' | $awk '{print "insert into foreign_datafile values ("$6",\x27"$8"\x27);"}' > target_foreign_datafile.sql
