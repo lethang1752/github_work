814ac9ecb46e75297aca69ca8960e4fbbb14f528
lethang1752 <79958184+lethang1752@users.noreply.github.com>
2023-10-09 18:17:48 +0700

    a

--- crossplatform_backup_restore/create_foreign_table.sh
+++ crossplatform_backup_restore/create_foreign_table.sh
@@ -3,7 +3,7 @@
 . migrate.properties
 
 sqlplus -S "/ as sysdba" << !
-create table foreign_datafile(file# int, name varchar(200)) tablespace system;
+create table foreign_datafile(file_id int, name varchar(200)) tablespace system;
 !
 
 cat rman_output_restore_full.log | grep 'restoring foreign file' | grep '+' | awk '{print "insert into foreign_datafile values ("$6",\x27"$8"\x27);"}' > foreign_datafile_table.sql
