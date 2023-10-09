d82b8bedf06226287221247bf4214224af1c47cf
lethang1752 <79958184+lethang1752@users.noreply.github.com>
2023-10-09 18:19:13 +0700

    a

--- crossplatform_backup_restore/prepare_backup1.sh
+++ crossplatform_backup_restore/prepare_backup1.sh
@@ -50,11 +50,12 @@ select 'allocate channel c' || level || ' device type disk;'
 from dual
 connect by rownum<=${target_parallel_degree}
 union all
-select 'recover from platform ''${src_platform_name}'' foreign datafilecopy ' || file_id ||
+select 'recover from platform ''${src_platform_name}'' foreign datafilecopy ' || fd.name ||
        ' from backupset ''${target_backup_dest}/' || lower(t.tablespace_name) || trim(to_char(row_number() over (partition by t.tablespace_name order by f.file_id),'00')) || '_${file_name}'';'
-from dba_data_files f, dba_Tablespaces t
+from dba_data_files f, dba_Tablespaces t, foreign_datafile fd
 where f.tablespace_name=t.tablespace_name
 and   t.CONTENTS='PERMANENT'
+and   fd.file_id=f.file_id
 and   t.tablespace_name not in ('SYSTEM','SYSAUX')
 union all
 select '}'
