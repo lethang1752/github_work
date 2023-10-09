e0091f7f1718e3ce173b3dbe0dffb6bd0e3db6a9
lethang1752 <79958184+lethang1752@users.noreply.github.com>
2023-10-09 20:33:57 +0700

    a

---
+++ crossplatform_backup_restore/target_foreign_datafile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+os=$(uname)
+if [[ "$os" == 'Linux' ]]; then
+	grep='grep'
+	awk='awk'
+else
+	grep='ggrep'
+	awk='nawk'
+fi
+
+sh migrate.properties
+
+sqlplus -S "/ as sysdba" << !
+create table foreign_datafile(file_id int, name varchar(200)) tablespace system;
+!
+
+cat rman_output_restore_full.log | $grep 'restoring foreign file' | $grep '+' | $awk '{print "insert into foreign_datafile values ("$6",\x27"$8"\x27);"}' > target_foreign_datafile.sql
+
+sqlplus -S "/ as sysdba" @target_foreign_datafile.sql
