d028e85c14ccb1afc349f8677d185399eaedf64a
lethang1752 <79958184+lethang1752@users.noreply.github.com>
2024-03-06 16:36:40 +0700

    a

---
+++ Tuning Fragment/move_table.sql
@@ -0,0 +1,113 @@
+CREATE OR REPLACE PROCEDURE move_table	(oldtablespace   IN VARCHAR2,
+                                        newtablespace   IN VARCHAR2)
+IS
+    a	VARCHAR2 (250);
+    CURSOR c1 IS
+          SELECT DISTINCT owner, SEGMENT_NAME, partition_name
+            FROM dba_segments
+           WHERE     tablespace_name = oldtablespace
+                 AND segment_type LIKE '%TABLE PARTITION%'
+        ORDER BY owner;
+
+    CURSOR c2 IS
+          SELECT DISTINCT owner, SEGMENT_NAME, segment_type
+            FROM dba_segments
+           WHERE     tablespace_name = oldtablespace
+                 AND segment_type LIKE '%TABLE%'
+        ORDER BY owner;
+
+    CURSOR c3 IS
+        SELECT l.owner           owner,
+               l.table_name      tablename,
+               l.column_name     columnname,
+               l.tablespace_name,
+               s.segment_name,
+               s.segment_type
+          FROM dba_lobs  l
+               JOIN dba_segments s ON l.segment_name = s.segment_name
+         WHERE     s.segment_type LIKE 'LOBSEGMENT'
+               AND s.tablespace_name = oldtablespace;
+
+    CURSOR c4 IS
+        SELECT table_owner,
+               table_name,
+               partition_name,
+               column_name
+          FROM dba_lob_partitions
+         WHERE tablespace_name = oldtablespace;
+BEGIN
+    FOR t1 IN c1
+    LOOP
+        BEGIN
+            a :=
+                   'alter table '
+                || t1.owner
+                || '.'
+                || t1.SEGMENT_NAME
+                || ' move partition '
+                || t1.partition_name
+                || ' tablespace '
+                || newtablespace
+				|| ';';
+        DBMS_OUTPUT.PUT_LINE (a);
+        EXCEPTION
+            WHEN OTHERS
+            THEN
+                CONTINUE;
+        END;
+    END LOOP;
+    FOR t1 IN c2
+    LOOP
+        BEGIN
+            a :=
+                   'alter table '
+                || t1.owner
+                || '.'
+                || t1.SEGMENT_NAME
+                || ' move tablespace '
+                || newtablespace
+				|| ';';
+            DBMS_OUTPUT.PUT_LINE (a);
+        EXCEPTION
+            WHEN OTHERS
+            THEN
+                CONTINUE;
+        END;
+    END LOOP;
+    FOR t1 IN c3
+    LOOP
+        BEGIN
+            a :=
+                   'alter table '
+                || t1.owner
+                || '.'
+                || t1.tablename
+                || ' move lob('
+                || t1.columnname
+                || ') store as (tablespace '
+                || newtablespace
+                || ');';
+            DBMS_OUTPUT.PUT_LINE (a);
+        EXCEPTION
+            WHEN OTHERS
+            THEN
+                CONTINUE;
+        END;
+    END LOOP;
+    FOR t1 IN c4
+    LOOP
+        a :=
+               'alter table '
+            || t1.TABLE_OWNER
+            || '.'
+            || t1.TABLE_NAME
+            || ' move partition '
+            || t1.partition_name
+            || ' lob ('
+            || t1.COLUMN_NAME
+            || ') store as basicfile (tablespace '
+            || newtablespace
+            || ');';
+        DBMS_OUTPUT.PUT_LINE (a);
+    END LOOP;
+END move_tablespace;
