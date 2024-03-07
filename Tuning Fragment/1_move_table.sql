f8986b4ff5bbe8f6c9fd687b717e2c1a3c4d9cdd
lethang1752 <79958184+lethang1752@users.noreply.github.com>
2024-03-07 14:26:20 +0700

    a

--- Tuning Fragment/1_move_table.sql
+++ Tuning Fragment/1_move_table.sql
@@ -1,163 +1,64 @@
-CREATE OR REPLACE PROCEDURE move_table (
-    oldtablespace IN VARCHAR2,
-    newtablespace IN VARCHAR2
-) IS
+/* Formatted on 2/20/2024 2:01:59 PM (QP5 v5.401) */
+CREATE OR REPLACE PROCEDURE move_table (oldtablespace   IN VARCHAR2,
+                                             newtablespace   IN VARCHAR2)
+IS
+    a   VARCHAR2 (250);
 
-    a VARCHAR2(250);
     CURSOR c1 IS
-    SELECT DISTINCT
-        owner,
-        segment_name,
-        partition_name
-    FROM
-        dba_segments
-    WHERE
-            tablespace_name = oldtablespace
-        AND segment_type LIKE '%TABLE PARTITION%'
-    ORDER BY
-        owner;
+          SELECT DISTINCT owner, SEGMENT_NAME, partition_name
+            FROM dba_segments
+           WHERE     tablespace_name = oldtablespace
+                 AND segment_type LIKE '%TABLE PARTITION%'
+        ORDER BY owner;
 
     CURSOR c2 IS
-    SELECT DISTINCT
-        owner,
-        segment_name,
-        segment_type
-    FROM
-        dba_segments
-    WHERE
-            tablespace_name = oldtablespace
-        AND segment_type LIKE '%TABLE%'
-    ORDER BY
-        owner;
-
-    CURSOR c3 IS
-    SELECT
-        l.owner       owner,
-        l.table_name  tablename,
-        l.column_name columnname,
-        l.tablespace_name,
-        s.segment_name,
-        s.segment_type
-    FROM
-             dba_lobs l
-        JOIN dba_segments s ON l.segment_name = s.segment_name
-    WHERE
-        s.segment_type LIKE 'LOBSEGMENT'
-        AND s.tablespace_name = oldtablespace;
-
-    CURSOR c4 IS
-    SELECT
-        table_owner,
-        table_name,
-        partition_name,
-        column_name
-    FROM
-        dba_lob_partitions
-    WHERE
-        tablespace_name = oldtablespace;
-
-    CURSOR c5 IS
-    SELECT
-        index_owner       owner,
-        index_name        indexname,
-        subpartition_name subpartitionname,
-        tablespace_name   tablespacename
-    FROM
-        dba_ind_subpartitions
-    WHERE
-        tablespace_name = oldtablespace
-    ORDER BY
-        owner;
-
+          SELECT DISTINCT owner, SEGMENT_NAME, segment_type
+            FROM dba_segments
+           WHERE     tablespace_name = oldtablespace
+                 AND segment_type LIKE '%TABLE%'
+        ORDER BY owner;
 BEGIN
-    FOR t1 IN c1 LOOP
+    FOR t1 IN c1
+    LOOP
         BEGIN
-            a := 'alter table '
-                 || t1.owner
-                 || '.'
-                 || t1.segment_name
-                 || ' move partition '
-                 || t1.partition_name
-                 || ' tablespace '
-                 || newtablespace
-                 || ';';
+            a :=
+                   'alter table '
+                || t1.owner
+                || '.'
+                || t1.SEGMENT_NAME
+                || ' move partition '
+                || t1.partition_name
+                || ' tablespace '
+                || newtablespace;
 
-            dbms_output.put_line(a);
+            EXECUTE IMMEDIATE a;
         EXCEPTION
-            WHEN OTHERS THEN
+            WHEN OTHERS
+            THEN
+                CONTINUE;
+        END;
+
+        DBMS_OUTPUT.PUT_LINE (a);
+    END LOOP;
+
+    FOR t1 IN c2
+    LOOP
+        BEGIN
+            a :=
+                   'alter table '
+                || t1.owner
+                || '.'
+                || t1.SEGMENT_NAME
+                || ' move tablespace '
+                || newtablespace;
+            DBMS_OUTPUT.PUT_LINE (a);
+
+            EXECUTE IMMEDIATE a;
+        EXCEPTION
+            WHEN OTHERS
+            THEN
                 CONTINUE;
         END;
     END LOOP;
-
-    FOR t1 IN c2 LOOP
-        BEGIN
-            a := 'alter table '
-                 || t1.owner
-                 || '.'
-                 || t1.segment_name
-                 || ' move tablespace '
-                 || newtablespace
-                 || ';';
-
-            dbms_output.put_line(a);
-        EXCEPTION
-            WHEN OTHERS THEN
-                CONTINUE;
-        END;
-    END LOOP;
-
-    FOR t1 IN c3 LOOP
-        BEGIN
-            a := 'alter table '
-                 || t1.owner
-                 || '.'
-                 || t1.tablename
-                 || ' move lob('
-                 || t1.columnname
-                 || ') store as (tablespace '
-                 || newtablespace
-                 || ');';
-
-            dbms_output.put_line(a);
-        EXCEPTION
-            WHEN OTHERS THEN
-                CONTINUE;
-        END;
-    END LOOP;
-
-    FOR t1 IN c4 LOOP
-        a := 'alter table '
-             || t1.table_owner
-             || '.'
-             || t1.table_name
-             || ' move partition '
-             || t1.partition_name
-             || ' lob ('
-             || t1.column_name
-             || ') store as basicfile (tablespace '
-             || newtablespace
-             || ');';
-
-        dbms_output.put_line(a);
-    END LOOP;
-
-    FOR t1 IN c5 LOOP
-        BEGIN
-            a := 'alter index '
-                 || t1.owner
-                 || '.'
-                 || t1.indexname
-                 || ' rebuild subpartition '
-                 || t1.subpartitionname
-                 || ' tablespace '
-                 || newtablespace
-                 || ' online';
-
-            dbms_output.put_line(a);
-        EXCEPTION
-            WHEN OTHERS THEN
-                CONTINUE;
-        END;
-    END LOOP;
-
-END move_table;
+    
+END move_table;
