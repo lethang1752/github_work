4a9806dd6e6fe96cffb13961e93831dc254fc6f5
Victor <lethang1752@outlook.com>
2024-03-07 15:05:26 +0700

    a

--- Tuning Fragment/1_move_table.sql
+++ Tuning Fragment/1_move_table.sql
@@ -1,60 +1,72 @@
 /* Formatted on 2/20/2024 2:01:59 PM (QP5 v5.401) */
-CREATE
-OR REPLACE PROCEDURE move_table (
+CREATE OR REPLACE PROCEDURE move_table (
     oldtablespace IN VARCHAR2,
     newtablespace IN VARCHAR2
-) IS a VARCHAR2 (250);
+) IS
 
-CURSOR c1 IS
-SELECT
-    DISTINCT owner,
-    SEGMENT_NAME,
-    partition_name
-FROM
-    dba_segments
-WHERE
-    tablespace_name = oldtablespace
-    AND segment_type LIKE '%TABLE PARTITION%'
-ORDER BY
-    owner;
+    a VARCHAR2(250);
+    CURSOR c1 IS
+    SELECT DISTINCT
+        owner,
+        segment_name,
+        partition_name
+    FROM
+        dba_segments
+    WHERE
+            tablespace_name = oldtablespace
+        AND segment_type LIKE '%TABLE PARTITION%'
+    ORDER BY
+        owner;
 
-CURSOR c2 IS
-SELECT
-    DISTINCT owner,
-    SEGMENT_NAME,
-    segment_type
-FROM
-    dba_segments
-WHERE
-    tablespace_name = oldtablespace
-    AND segment_type LIKE '%TABLE%'
-ORDER BY
-    owner;
+    CURSOR c2 IS
+    SELECT DISTINCT
+        owner,
+        segment_name,
+        segment_type
+    FROM
+        dba_segments
+    WHERE
+            tablespace_name = oldtablespace
+        AND segment_type LIKE '%TABLE%'
+    ORDER BY
+        owner;
 
-BEGIN FOR t1 IN c1 LOOP BEGIN a := 'alter table ' || t1.owner || '.' || t1.SEGMENT_NAME || ' move partition ' || t1.partition_name || ' tablespace ' || newtablespace;
+BEGIN
+    FOR t1 IN c1 LOOP
+        BEGIN
+            a := 'alter table '
+                 || t1.owner
+                 || '.'
+                 || t1.segment_name
+                 || ' move partition '
+                 || t1.partition_name
+                 || ' tablespace '
+                 || newtablespace;
 
-EXECUTE IMMEDIATE a;
+            EXECUTE IMMEDIATE a;
+        EXCEPTION
+            WHEN OTHERS THEN
+                CONTINUE;
+        END;
 
-EXCEPTION
-WHEN OTHERS THEN CONTINUE;
+        dbms_output.put_line(a);
+    END LOOP;
 
-END;
+    FOR t1 IN c2 LOOP
+        BEGIN
+            a := 'alter table '
+                 || t1.owner
+                 || '.'
+                 || t1.segment_name
+                 || ' move tablespace '
+                 || newtablespace;
 
-DBMS_OUTPUT.PUT_LINE (a);
-
-END LOOP;
-
-FOR t1 IN c2 LOOP BEGIN a := 'alter table ' || t1.owner || '.' || t1.SEGMENT_NAME || ' move tablespace ' || newtablespace;
-
-DBMS_OUTPUT.PUT_LINE (a);
-
-EXECUTE IMMEDIATE a;
-
-EXCEPTION
-WHEN OTHERS THEN CONTINUE;
-
-END;
-
-END LOOP;
+            dbms_output.put_line(a);
+            EXECUTE IMMEDIATE a;
+        EXCEPTION
+            WHEN OTHERS THEN
+                CONTINUE;
+        END;
+    END LOOP;
 
 END move_table;
--- Tuning Fragment/2_move_lob.sql
+++ Tuning Fragment/2_move_lob.sql
@@ -1,87 +1,78 @@
-CREATE OR REPLACE PROCEDURE move_lob (
+CREATE
+OR REPLACE PROCEDURE move_lob (
     oldtablespace IN VARCHAR2,
     newtablespace IN VARCHAR2
-) IS
+) IS executestatement VARCHAR2(500);
 
-    executestatement VARCHAR2(500);
-    isexecute        NUMBER;
-    currenttable     VARCHAR(50);
-    isnewtable       NUMBER;
-    CURSOR c IS
-    SELECT
-        l.owner       owner,
-        l.table_name  tablename,
-        l.column_name columnname,
-        l.tablespace_name,
-        s.segment_name,
-        s.segment_type
-    FROM
-             dba_lobs l
-        JOIN dba_segments s ON l.segment_name = s.segment_name
-    WHERE
-        s.segment_type LIKE 'LOBSEGMENT'
-        AND s.tablespace_name = oldtablespace
-        AND l.column_name NOT LIKE '%"%'
-    ORDER BY
-        1,
-        2;
+isexecute NUMBER;
 
-BEGIN
-    currenttable := 'current';
-    isexecute := 0;
-    isnewtable := 0;
-    executestatement := 'statement';
-    FOR t1 IN c LOOP
-        BEGIN
-            IF ( currenttable != t1.tablename ) THEN
-                currenttable := t1.tablename;
-                isnewtable := 1;
-                IF ( executestatement = 'statement' ) THEN
-                    isexecute := 0;
-                ELSE
-                    isexecute := 1;
-                END IF;
+currenttable VARCHAR(50);
 
-            ELSE
-                isnewtable := 0;
-            END IF;
+isnewtable NUMBER;
 
-            IF ( isexecute = 1 ) THEN
-                dbms_output.put_line(executestatement);
-                --EXECUTE IMMEDIATE executeStatement;
-                executestatement := 'statement';
-                isexecute := 0;
-            END IF;
+CURSOR c IS
+SELECT
+    l.owner owner,
+    l.table_name tablename,
+    l.column_name columnname,
+    l.tablespace_name,
+    s.segment_name,
+    s.segment_type
+FROM
+    dba_lobs l
+    JOIN dba_segments s ON l.segment_name = s.segment_name
+WHERE
+    s.segment_type LIKE 'LOBSEGMENT'
+    AND s.tablespace_name = oldtablespace
+    AND l.column_name NOT LIKE '%"%'
+ORDER BY
+    1,
+    2;
 
-            IF isnewtable = 1 THEN
-                executestatement := 'alter table '
-                                    || t1.owner
-                                    || '.'
-                                    || t1.tablename
-                                    || ' move lob('
-                                    || t1.columnname
-                                    || ') store as (tablespace '
-                                    || newtablespace
-                                    || ');';
-            ELSE
-                executestatement := executestatement
-                                    || chr(10)
-                                    || 'alter table '
-                                    || t1.owner
-                                    || '.'
-                                    || t1.tablename
-                                    || ' move lob('
-                                    || t1.columnname
-                                    || ') store as (tablespace '
-                                    || newtablespace
-                                    || ');';
-            END IF;
+BEGIN currenttable := 'current';
 
-        EXCEPTION
-            WHEN OTHERS THEN
-                CONTINUE;
-        END;
-    END LOOP;
+isexecute := 0;
 
-    dbms_output.put_line(executestatement);
+isnewtable := 0;
+
+executestatement := 'statement';
+
+FOR t1 IN c LOOP BEGIN IF (currenttable != t1.tablename) THEN currenttable := t1.tablename;
+
+isnewtable := 1;
+
+IF (executestatement = 'statement') THEN isexecute := 0;
+
+ELSE isexecute := 1;
+
+END IF;
+
+ELSE isnewtable := 0;
+
+END IF;
+
+IF (isexecute = 1) THEN dbms_output.put_line(executestatement);
+
+--EXECUTE IMMEDIATE executeStatement;
+executestatement := 'statement';
+
+isexecute := 0;
+
+END IF;
+
+IF isnewtable = 1 THEN executestatement := 'alter table ' || t1.owner || '.' || t1.tablename || ' move lob(' || t1.columnname || ') store as (tablespace ' || newtablespace || ');';
+
+ELSE executestatement := executestatement || chr(10) || 'alter table ' || t1.owner || '.' || t1.tablename || ' move lob(' || t1.columnname || ') store as (tablespace ' || newtablespace || ');';
+
+END IF;
+
+EXCEPTION
+WHEN OTHERS THEN CONTINUE;
+
+END;
+
+END LOOP;
+
+dbms_output.put_line(executestatement);
+
 END move_lob;
