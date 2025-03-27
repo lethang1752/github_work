73352aadb0ce0a7d225a36519ecdd604f24d531c
lethang1752 <79958184+lethang1752@users.noreply.github.com>
2025-03-27 17:33:20 +0700

    a

--- Contain Everything/create_vm_virtualbox_shell.ps1
+++ Contain Everything/create_vm_virtualbox_shell.ps1
@@ -1,6 +1,8 @@
-#!/bin/bash
-MACHINENAME=$1
-FOLDER=E:\VirtualMachine
+$MACHINENAME="linux_os"
+$FOLDER="E:\VirtualMachine"
+
+#Restart adapter
+Restart-NetAdapter -InterfaceDescription "VirtualBox Host-Only Ethernet Adapter"
 
 #Create VM
 VBoxManage createvm --name $MACHINENAME --ostype Oracle7_64 --register --basefolder $FOLDER
@@ -9,11 +11,11 @@ VBoxManage modifyvm $MACHINENAME --ioapic on
 VBoxManage modifyvm $MACHINENAME --memory 8192 --vram 128
 VBoxManage modifyvm $MACHINENAME --nic1 hostonly --host-only-adapter1="VirtualBox Host-Only Ethernet Adapter"
 #Create Disk and connect Iso
-VBoxManage createhd --filename $FOLDER/$MACHINENAME/$MACHINENAME_disk.vdi --size 80000 --format VDI
+VBoxManage createhd --filename $FOLDER\$MACHINENAME\${MACHINENAME}_disk.vdi --size 80000 --format VDI
 VBoxManage storagectl $MACHINENAME --name "SATA Controller" --add sata --controller IntelAhci
-VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  $FOLDER/$MACHINENAME/$MACHINENAME_disk.vdi
+VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  $FOLDER\$MACHINENAME\${MACHINENAME}_disk.vdi
 VBoxManage storagectl $MACHINENAME --name "IDE Controller" --add ide --controller PIIX4
-VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium D:/Iso/OracleLinux-R7-U9-Server-x86_64-dvd.iso
+VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium D:\Iso\OracleLinux-R7-U9-Server-x86_64-dvd.iso
 VBoxManage modifyvm $MACHINENAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
 
 #Enable RDP
@@ -21,4 +23,7 @@ VBoxManage modifyvm $MACHINENAME --vrde on
 VBoxManage modifyvm $MACHINENAME --vrdemulticon on --vrdeport 10001
 
 #Start the VM
-VBoxHeadless --startvm $MACHINENAME
+VBoxManage startvm $MACHINENAME
+
+#Close
+exit
